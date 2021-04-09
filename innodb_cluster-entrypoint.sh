690767bba956a0867c1e31708930bc376bb6740c
Tomas Kudelka <tomas@appsdevteam.com>
2021-04-09 11:28:45 +0200

    fix

--- innodb_cluster-entrypoint.sh
+++ innodb_cluster-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
 
+set -x
 
 # if command starts with an option, save them as CMD arguments
 if [ "${1:0:1}" = '-' ]; then
@@ -12,12 +12,12 @@ if [ -n "$MYSQL_ROOT_PASSWORD" -a -f "$MYSQL_ROOT_PASSWORD" ]; then
 	MYSQL_ROOT_PASSWORD="$(cat $MYSQL_ROOT_PASSWORD)"
 fi
 
-# If we're setting up a router 
+# If we're setting up a router
 if [ "$NODE_TYPE" = 'router' ]; then
 
         echo 'Setting up a new router instance...'
 
-        # we need to ensure that they've specified a boostrap URI 
+        # we need to ensure that they've specified a boostrap URI
         if [ -z "$MYSQL_HOST" -a -z "$MYSQL_PASSWORD" ]; then
                 echo >&2 'error: You must specify a value for MYSQL_HOST and MYSQL_PASSWORD (MYSQL_USER=root is the default) when setting up a router'
                 exit 1
@@ -38,16 +38,16 @@ if [ "$NODE_TYPE" = 'router' ]; then
         # We'll use the hostname as the router instance name
 	HOSTNAME=$(hostname)
 
-        # first we need to see if the cluster metadata already exists 
-	set +e
+        # first we need to see if the cluster metadata already exists
+
         metadata_exists=$(mysqlsh --uri="$MYSQL_USER"@"$MYSQL_HOST":"$MYSQL_PORT" -p"$MYSQL_ROOT_PASSWORD" --no-wizard --js -i -e "dba.getCluster( '${CLUSTER_NAME}' )" 2>&1 | grep "<Cluster:$CLUSTER_NAME>")
-        set -e
+
 
 	# We need to get the host:port combination for the primary node (or just the first node when NOT in single primary mode)
         HOSTPORT=$(mysql --no-defaults -h "$MYSQL_HOST" -P"$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_ROOT_PASSWORD" -nsLNE -e "select CONCAT(member_host, ':', member_port) as primary_host from performance_schema.replication_group_members where member_state='ONLINE' and member_id=(IF((select @grpm:=variable_value from performance_schema.global_status where variable_name='group_replication_primary_member') = '', member_id, @grpm)) limit 1" 2>/dev/null | grep -v '*')
 
         if [ -z "$metadata_exists" ]; then
-		# Then let's create the innodb cluster metadata 
+		# Then let's create the innodb cluster metadata
 		output=$(mysqlsh --uri="$MYSQL_USER"@"$HOSTPORT" -p"$MYSQL_ROOT_PASSWORD" --no-wizard --js -i -e "dba.createCluster('${CLUSTER_NAME}', {adoptFromGR: true})")
 	fi
 
@@ -58,27 +58,27 @@ if [ "$NODE_TYPE" = 'router' ]; then
 		echo >&2 "$output"
 		exit 1
 	fi
-       
-        # bug (?) in Router 2.1.3 didn't set file permissions based on --user value 
+
+	# bug (?) in Router 2.1.3 didn't set file permissions based on --user value
 	chown -R mysql:mysql "/var/lib/mysqlrouter"
 
-        # now that we've bootstrapped the setup, let's start the process
-        CMD="mysqlrouter --user=mysql"
+	# now that we've bootstrapped the setup, let's start the process
+	CMD="mysqlrouter --user=mysql"
 
-# Let's setup a mysql server instance normally 
+# Let's setup a mysql server instance normally
 else
-	if [ -z "$BOOTSTRAP" -a -z "$GROUP_NAME" ]; then 
+	if [ -z "$BOOTSTRAP" -a -z "$GROUP_NAME" ]; then
 		echo >&2 'error: You must either use BOOTSTRAP=1 to start a new cluster--where a new group name UUID will be generated--or you must specify a valid UUID for the GROUP_NAME that you wish to join'
-	        exit 1
+		exit 1
 	fi
 
-        # let's generate a random server_id value; it can be any unsigned 32 bit int
-        [ -z "$SERVER_ID" ] && SERVER_ID=$((RANDOM % 1000))
+	# let's generate a random server_id value; it can be any unsigned 32 bit int
+	[ -z "$SERVER_ID" ] && SERVER_ID=$((RANDOM % 1000))
 
-        CMD="mysqld"
+	CMD="mysqld"
 
-        # We'll use this variable to manage the mysqld args 
-        MYSQLD_ARGS="--server_id=$SERVER_ID"
+	# We'll use this variable to manage the mysqld args
+	MYSQLD_ARGS="--log-error=/var/log/mysqld.log --server_id=$SERVER_ID"
 
 	# Test we're able to startup without errors. We redirect stdout to /dev/null so
 	# only the error messages are left.
@@ -105,15 +105,15 @@ else
 		if [ ! -d "$DATADIR/mysql" ] || [ -z "$GROUP_SEEDS" ]; then
 			GR_ARGS="$GR_ARGS --group_replication_bootstrap_group=ON"
 
-			# If a group name hasn't been specified anywhere, let's finally auto generate one 
+			# If a group name hasn't been specified anywhere, let's finally auto generate one
 			if [ -z "$GROUP_NAME" ]; then
 				GROUP_NAME=$(uuidgen)
 
 				# Let's persist the group_name since the env variable and config option was not set
 				# This will allow for restarting the container w/o bootstrapping a new/second cluster
 				echo "loose-group-replication-group-name=$GROUP_NAME" >> /etc/mysql/my.cnf
-			fi	
-		
+			fi
+
 			echo >&1 "info: Bootstrapping Group Replication cluster using --group_replication_group_name=$GROUP_NAME"
 			echo >&1 "  You will need to specify GROUP_NAME=$GROUP_NAME if you want to add another node to this cluster"
 		fi
@@ -124,7 +124,7 @@ else
 		echo >&1 "info: attempting to join the $GROUP_NAME group using $GROUP_SEEDS as seeds"
 	fi
 
-	# Let's validate the group_name, as it needs to be a valid UUID 
+	# Let's validate the group_name, as it needs to be a valid UUID
 	is_valid_uuid="$(echo $GROUP_NAME | tr '[:upper:]' '[:lower:]' | awk '$1 ~ /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/ { print $1; exit }')"
 	if [ -z "$is_valid_uuid" ]; then
 		echo >&2 'error: Specified GROUP_NAME is not a valid UUID'
@@ -166,7 +166,7 @@ else
 		fi
 
 		mysql_tzinfo_to_sql /usr/share/zoneinfo | "${mysql[@]}" mysql
-		
+
 		if [ ! -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
 			MYSQL_ROOT_PASSWORD="$(pwmake 128)"
 			echo "GENERATED ROOT PASSWORD: $MYSQL_ROOT_PASSWORD"
@@ -174,10 +174,9 @@ else
 		"${mysql[@]}" <<-EOSQL
 			-- What's done in this file shouldn't be replicated
 			SET @@SESSION.SQL_LOG_BIN=0;
-			DELETE FROM mysql.user WHERE user NOT IN ('mysql.session', 'mysql.sys', 'mysqlxsys') OR host NOT IN ('localhost');
-			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
-			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-			DROP DATABASE IF EXISTS test ;
+			DELETE FROM mysql.user WHERE user NOT IN ('mysql.infoschema', 'mysql.session', 'mysql.sys', 'root') OR host NOT IN ('localhost');
+			CREATE USER 'healthchecker'@'localhost' IDENTIFIED BY 'healthcheckpass';
+			ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 			FLUSH PRIVILEGES ;
 		EOSQL
 		if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
@@ -210,7 +209,7 @@ else
 			echo
 		done
 
-                # let's remove any binary logs or GTID metadata that may have been generated 
+                # let's remove any binary logs or GTID metadata that may have been generated
                 echo 'RESET MASTER ;' | "${mysql[@]}"
 
                 # lastly we need to setup the recovery channel with a valid username/password
@@ -235,8 +234,8 @@ fi
 touch "$HOME"/.my.cnf
 cat > "$HOME/.my.cnf" <<EOCF
 [client]
-user=root
-password=${MYSQL_ROOT_PASSWORD}
+user=healthchecker
+password=healthcheckpass
 EOCF
 
 exec $CMD
